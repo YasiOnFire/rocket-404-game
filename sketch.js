e9a158617224ae54554a75f0bb3accf20c0f59f9
Yasio <baszczok@yasio.pl>
2018-09-11 00:00:18 +0200

    restart game, text, style fixes, new point mechanics

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 let y_axis = 0
 let y_speed = 0.9
 let score = 0
+let hiscore = 0
 let endGame = false
 let playing = false
+let readyToPlay = false
 let engine
 let world
 let ground
@@ -10,12 +12,32 @@ let option
 let rocket
 let rocketTexture
 let fuel = 100
-const TIME_LIMIT = 3000
+const TIME_LIMIT = 5000
 const Engine = Matter.Engine
 const Render = Matter.Render
 const World = Matter.World
 const Bodies = Matter.Bodies
 const Body = Matter.Body
+
+document.getElementById('restartGame').addEventListener('click', function (e) {
+	fuel = 100
+	score = 0
+	endGame = false
+	playing = false
+	readyToPlay = false
+	setTimeout(function () {
+		readyToPlay = true
+	}, 6000)
+	rocket.previousHeight = height
+	document.querySelector('#score span').innerHTML = score
+	document.querySelector('#time span').innerHTML = fuel
+	document.getElementById('restartGame').classList.add('hidden')
+});
+
+if (localStorage.getItem('hiscorerocket')) {
+	hiscore = localStorage.getItem('hiscorerocket')
+	document.querySelector('#hiscore span').innerHTML = hiscore
+}
 
 function preload() {
 	rocketTexture = loadImage('rocket.png');
@@ -28,6 +50,9 @@ function setup() {
 	Engine.run(engine)
 	ground = new Ground(height - 100)
 	rocket = new Rocket(width - (width / 3), 10, 50, 100)
+	setTimeout(function () {
+		readyToPlay = true
+	}, 2000)
 }
 
 function draw() {
@@ -39,27 +64,31 @@ function draw() {
 }
 
 function mousePressed() {
-	if (endGame) return
+	if (!readyToPlay) return
+	if (endGame) {
+		document.getElementById('restartGame').classList.remove('hidden')
+		return
+	}
 	if (!playing) {
 		playing = true
-		gameTime = setInterval(() => {
-			fuel -= 10
-			document.querySelector('#time span').innerHTML = fuel
-		}, TIME_LIMIT / 10)
-		setTimeout(() => {
-			endGame = true
-			clearInterval(gameTime)
-		}, TIME_LIMIT)
 	}
-	rocket.go()
-	score += 1
-	document.querySelector('#score span').innerHTML = score
-	document.querySelector('#time span').innerHTML = fuel
+	if (fuel <= 0) {
+		fuel = 0
+		endGame = true
+	} else {
+		rocket.go()
+		fuel -= Math.round(random(0, 4))
+		document.querySelector('#time span').innerHTML = fuel
+	}
+	// document.querySelector('#time span').innerHTML = fuel
 }
 
 function Rocket(x, y, wid, hei) {
-	this.body = Bodies.rectangle(x, y, wid, hei)
+	this.body = Bodies.rectangle(x, y, wid, hei, {
+		restitution: .6
+	})
 	this.currSpeed = 1
+	this.previousHeight = height
 
 	World.add(world, this.body)
 
@@ -71,7 +100,7 @@ function Rocket(x, y, wid, hei) {
 		strokeWeight(3)
 		fill('#bdbcbd')
 		translate(pos.x, pos.y)
-		rect(0, 50, wid, hei)
+		// rect(0, 50, wid, hei)
 		image(rocketTexture, 0, 50, wid, hei);
 		pop()
 	}
@@ -82,9 +111,16 @@ function Rocket(x, y, wid, hei) {
 		}
 
 		Body.applyForce(this.body, this.body.position, f)
-
-		// this.body.position.y -= this.currSpeed * 3
-		// console.log('this.body.position.y: ', this.body.position)
+		if (this.body.position.y < this.previousHeight) {
+			this.previousHeight = this.body.position.y
+			score = Math.round(height - this.body.position.y) * 10
+			document.querySelector('#score span').innerHTML = score
+			if (score > hiscore) {
+				hiscore = score
+				localStorage.setItem('hiscorerocket', hiscore)
+				document.querySelector('#hiscore span').innerHTML = hiscore
+			}
+		}
 	}
 }
 
